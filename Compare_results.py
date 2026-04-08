6de6097c2901a3faeb776509dd60c6aff63515d8
Jcsegertudelft <jcsegers@tudelft.nl>
2026-04-08 17:48:01 +0200

    Compare class specific precision, recall, F1

--- Compare_results.py
+++ Compare_results.py
@@ -3,25 +3,40 @@ import numpy as np
 from ultralytics import YOLO
 from statistics import harmonic_mean
 from pprint import pprint
+import pandas as pd
 
 def precis_and_recall(model_path, image_folder, label_folder):
     model = YOLO(model_path)
     pred_list = []
     true_list = []
-    for img in os.listdir(image_folder):
+    pred_cls_arr = np.full((5,len(os.listdir(image_folder))), False)
+    true_cls_arr = np.full((5,len(os.listdir(image_folder))), False)
+    for i,img in enumerate(os.listdir(image_folder)):
         #Obtain prediction: is there trash in image
         res = model(os.path.join(image_folder,img))[0]
-        predicted = len(res.boxes.conf) != 0
-        pred_list.append(predicted)
+        any_predicted = len(res.boxes.conf) != 0
+        pred_list.append(any_predicted)
+        if any_predicted:
+            predicted_classes = res.boxes.cls
+            for cls in predicted_classes:
+                cls = int(cls)
+                pred_cls_arr[cls][i]=True
+
+
 
         #Obtain ground truth: is there trash in image
         label_filename = img.replace('.jpg','.txt')
         label_path = os.path.join(label_folder,label_filename)
         with open(label_path,'r') as f:
-            true = len(f.readlines()) != 0
-        true_list.append(true)
+            lines = f.readlines()
+            any_true = len(lines) != 0
+            if any_true:
+                for line in lines:
+                    cls = int(line.strip().split(' ')[0])
+                    true_cls_arr[cls][i]=True
+        true_list.append(any_true)
 
-    #Calculate precision, recall and F1_score
+    #Calculate general precision, recall and F1_score
     pred_array = np.array(pred_list)
     true_array = np.array(true_list)
     true_pos_count = np.sum(pred_array[true_array])
@@ -31,7 +46,28 @@ def precis_and_recall(model_path, image_folder, label_folder):
     precision = true_pos_count/(true_pos_count+false_pos_count)
     recall = true_pos_count/(true_pos_count+false_neg_count)
     f1_score = harmonic_mean([precision, recall])
-    return precision, recall, f1_score
+
+    cls_precision = []
+    cls_recall = []
+    cls_f1_score = []
+    print(np.sum(pred_cls_arr),np.sum(true_cls_arr))
+    for i in range(5):
+        cls_pred = pred_cls_arr[i]
+        cls_true = true_cls_arr[i]
+        true_pos_count = np.sum(cls_true[cls_pred])
+        false_pos_count = np.sum(cls_pred[~cls_true])
+        false_neg_count = np.sum(cls_true[~cls_pred])
+        cls_precision.append(true_pos_count/(true_pos_count+false_pos_count))
+        cls_recall.append(true_pos_count/(true_pos_count+false_neg_count))
+        cls_f1_score.append(harmonic_mean([cls_precision[i], cls_recall[i]]))
+    cls_df = pd.DataFrame()
+    cls_df['Class'] = list(range(5))
+    cls_df['Precision'] = cls_precision
+    cls_df['Recall'] = cls_recall
+    cls_df['F1_score'] = cls_f1_score
+    cls_df.to_csv(model_path.replace('.pt','.csv'))
+
+    return precision, recall, f1_score,
 
 if __name__ == "__main__":
     image_dir = 'images/val'
--- Plot_results.py
+++ Plot_results.py
@@ -3,12 +3,11 @@ import pandas as pd
 
 def plot_results(results_file):
     df = pd.read_csv(results_file)
-    print(df.columns)
     fig, (ax1,ax2) = plt.subplots(1,2, sharex = True)
     fig.set_size_inches(8,5)
     fig.supxlabel('Epoch', fontsize = 15)
 
-    ax1.set_xlim(0,101)
+    ax1.set_xlim(0,max(df.index)+2)
 
     ax1.plot(df['epoch'],df['train/box_loss'],color='red', marker = 'o',markersize = 4,alpha = 0.7)
     ax1.plot(df['epoch'],df['val/box_loss'],color='blue', marker = 'o',markersize = 4,alpha = 0.7)
@@ -19,8 +18,9 @@ def plot_results(results_file):
     ax2.legend(fontsize = 13)
     ax2.set_title('Classification loss', size = 16, weight = 'bold')
     plt.tight_layout()
-    plt.savefig('loss_plot.png',dpi=300)
+    plt.savefig(f'loss_plot{results_file.rstrip('.csv')}.png',dpi=300)
 
 
 if __name__ == '__main__':
     plot_results('results_run1.csv')
+    plot_results('results_run2.csv')
